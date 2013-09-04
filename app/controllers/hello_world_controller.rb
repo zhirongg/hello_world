80475f11724f3fb70d49559a65bbd54860b76ecd
Alec Hoey <alechoey@gmail.com>
2013-09-04 13:22:47 -0700

    Initial commit

---
+++ app/controllers/hello_world_controller.rb
@@ -0,0 +1,5 @@
+class HelloWorldController < ApplicationController
+  def index
+    render 'index'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'efe6028af4b41d9bcdbae1749f100af87253dfc5d9e8d6377c038fa0538e25d2c4800582764418e2319e02987c667f727ad27e331550985de06a98314f30232f'
